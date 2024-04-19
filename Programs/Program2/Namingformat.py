75a0a053b5c44e11c5ed6c7472d6109268972778
BurntMuffins <aidanjs1208@gmail.com>
2024-04-18 20:12:57 -0500

    Created Program2 Project

---
+++ Programs/Program2/Namingformat.py
@@ -0,0 +1,5 @@
+# Provide your information as the values of these variables:
+myName = 'first_name, last_name'
+myTechID = '0000000'
+myTechEmail = 'abc123' #only your email id omit @latech.edu
+###########################################################
