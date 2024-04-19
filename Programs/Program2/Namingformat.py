624a5e82f7be3b901a9ab78c0dd9dd01ca64c6b8
BurntMuffins <aidanjs1208@gmail.com>
2024-04-18 20:40:48 -0500

    Removed Namingformat.py from In_Class_Work and Programs/Program1/Program2 directories

--- Programs/Program2/Namingformat.py
+++
@@ -1,5 +0,0 @@
-# Provide your information as the values of these variables:
-myName = 'first_name, last_name'
-myTechID = '0000000'
-myTechEmail = 'abc123' #only your email id omit @latech.edu
-###########################################################
